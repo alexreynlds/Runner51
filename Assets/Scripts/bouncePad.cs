2b290d0e5e437573b87e95f0040c0a9e162efa2a
Alex Reynolds <ajrey@live.co.uk>
2022-05-24 05:14:27 +0100

    Push 4.75
    Broke movement
    Added BOXOPHOBIC skies
    bounce pad + atom sfx

--- Assets/Scripts/bouncePad.cs
+++ Assets/Scripts/bouncePad.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class bouncePad : MonoBehaviour
 {
+    public AudioSource soundEffect;
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.name == ("Player")){
             Rigidbody otherRB = other.rigidbody;
             otherRB.AddExplosionForce(1000, other.contacts[0].point, 10);
+            soundEffect.Play();
         }
     }
 }

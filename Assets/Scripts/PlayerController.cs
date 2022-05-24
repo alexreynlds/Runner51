e963a30f93996e1f3cf9964bf4251b94cbce4606
Alex Reynolds <ajrey@live.co.uk>
2022-05-24 12:47:59 +0100

    Push 5
    Fixed Movement
    Added Lane Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -99,6 +99,9 @@ public class playerController : MonoBehaviour
         //Lane Movement
         if(gameMode == 1){
             float laneMove = context.ReadValue<Vector2>().x;
+            if(context.performed){
+                changeLane(laneMove);
+            }
             // Controls.Player.Movement.performed += _ => Debug.Log(context.ReadValue<Vector2>().x);
             // Controls.Player.Movement.triggered += _ => changeLane(laneMove);
             // if(Controls.Player.Movement.performed){
